b2f86e737edddc5bebaa9618c0e5d455ac3b0173
Mesi14 <Mesi18@proton.me>
2025-04-10 14:55:52 +0300

    :sparkles: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react"
 import './App.css'
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
-    <Button variant="surface">Some test button</Button>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"` 
+    }}>
+      <GridItem area="nav"><NavBar /></GridItem>
+      <Show above='lg'>
+        <GridItem area="aside" bg="gold">Aside</GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">Main</GridItem>
+    </Grid>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
-import { Provider } from "./components/ui/provider"
+import './index.css';
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider> 
+    </ChakraProvider> 
   </React.StrictMode>,
 )
